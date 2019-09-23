7632cd3d51964a8ff8da6dee0a4d76016810ad74
Cristian Cereseto <cristiancereseto@gmail.com>
2019-09-23 15:51:04 -0300

    added modal inside entry details

--- components/EntryDetails.js
+++ components/EntryDetails.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, Modal, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 import { Button, Card, Paragraph, Title } from 'react-native-paper';
 import moment from 'moment';
 
 export default class EntryDetails extends Component {
     constructor(props) {
         super(props);
+        this.state = { modalVisible: false };
+        this.toggleModalVisibility = this.toggleModalVisibility.bind(this);
     }
 
     render() {
@@ -18,22 +20,45 @@ export default class EntryDetails extends Component {
                     title='Go back'
                     onPress={() => this.props.navigation.goBack()}
                 />
-                <Card>
-                    <Card.Content styles={styles.container}>
-                        <Title>{entry.title} DETAILS {entry.isRead}</Title>
-                        <Paragraph>{entry.author} - {moment(entry.created_utc).fromNow()}</Paragraph>
-                    </Card.Content>
-                    {this.renderImage(entry.thumbnail)}
-                    <Card.Actions styles={styles.actionsContainer}>
-                        <Text>{entry.num_comments} comments</Text>
-                    </Card.Actions>
-                </Card>
+                <View styles={styles.container}>
+                    <Title>{entry.title} DETAILS {entry.isRead}</Title>
+                    <Paragraph>{entry.author} - {moment(entry.created_utc).fromNow()}</Paragraph>
+                    <Modal
+                        animationType="slide"
+                        transparent={false}
+                        visible={this.state.modalVisible}>
+                        <View style={{marginTop: 22}}>
+                            <View style={styles.modalContent}>
+                                <Image source={{uri:  entry.thumbnail}} style={{width: 400, height: 400}} />
+                                <TouchableHighlight
+                                    onPress={this.toggleModalVisibility.bind(false)}>
+                                    <Text>Hide Modal</Text>
+                                </TouchableHighlight>
+                            </View>
+                        </View>
+                    </Modal>
+                </View>
+                {this.renderImage(entry.thumbnail)}
+                <View styles={styles.actionsContainer}>
+                    <Text>{entry.num_comments} comments</Text>
+                </View>
             </View>
         )
     }
 
     renderImage(thumbnail) {
-        return (thumbnail) ? <Card.Cover source={{uri: thumbnail}} /> : null;
+        return (thumbnail) ?
+            (
+                <TouchableHighlight onPress={this.toggleModalVisibility.bind(this, true)}>
+                    <Image source={{uri: thumbnail}} style={{width: 200, height: 200}} />
+                </TouchableHighlight>
+            ) :
+            null;
+    }
+
+    toggleModalVisibility(visible) {
+        this.setState({modalVisible: visible});
+        console.log('toggleModalVisibility?', this.state.modalVisible);
     }
 }
 
@@ -46,5 +71,10 @@ const styles = StyleSheet.create({
         display: 'flex',
         flex: 1,
         justifyContent: 'space-between',
+    },
+    modalContent: {
+        alignItems: 'center',
+        display: 'flex',
+        justifyContent: 'center'
     }
 });
